174f0eee368ac480f07c527a4b14f8694503faf0
sahil <sahil2278.be23@chitkara.edu.in>
2025-07-29 15:33:30 +0530

    new

--- BEE-main/Lec11/index.js
+++ BEE-main/Lec11/index.js
@@ -12,8 +12,7 @@
 // console.log(el3[0]);
 let el4= document.querySelector("p");//. for class, # for id
 console.log(el4);
-let el5= document.querySelectorAll(".item");
-console.log(el5);
+
 let el6= document.querySelectorAll(".item");
 console.log(el6);
 let ul = document.querySelector("#container");
@@ -29,5 +28,34 @@ console.log(data);
 el4.innerText = "Hello World";
 let data2 = ul.innerHTML;
 console.log(data2);
+let data3 = ul.innerText;
+let data4 = ul.textContent;
+console.log(data3);
+console.log(data4);
+
+ul.innerHTML = `<li class = "item">Item 5</li>
+                <li class = "item">Item 6</li>
+                 <li class = "item">Item 7</li>`;
+     
+
+/*
+getAttribute
+setAttribute
+classList
+*/
+let ele5=document.querySelector(".item");
+console.dir(ele5);
+console.dir(ele5.getAttribute("id"));
+console.dir(ele5.getAttribute("class"));
+ele5.setAttribute("id","js");
+console.log(ele5.getAttribute("id"));
+ele5.classList.add("delete")
+console.log(ele5.classList.contains("delete"));
+ele5.classList.remove("item");
+console.log(ele5.classList);
+
+
+
+
 
 
