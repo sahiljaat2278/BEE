a280acc454cc3b7f5516b8a9fa88159f9344e5b2
sahil <sahil2278.be23@chitkara.edu.in>
2025-07-30 14:55:24 +0530

    new

--- BEE-main/Lec12/index.js
+++ BEE-main/Lec12/index.js
@@ -1,10 +1,21 @@
 const colors = [
-    "orange", "lightblue", "lightgreen", "pink", "purple",
-    "yellow", "red", "teal", "brown", "gold"
-];
+      "orange", "lightblue", "lightgreen", "pink", "purple",
+      "yellow", "red", "teal", "brown", "gold"
+    ];
 
-document.getElementById('generateBtn').onclick = function() {
-    const box = document.getElementById('box');
-    const randomColor = colors[Math.floor(Math.random() * colors.length)];
-    box.style.backgroundColor = randomColor;
-};
+    const box = document.getElementById("box");
+    let interval;
+
+    document.getElementById("generateBtn").onclick = function () {
+      if (interval) return; 
+
+      interval = setInterval(() => {
+        const color = colors[Math.floor(Math.random() * colors.length)];
+        box.style.backgroundColor = color;
+      }, 500);
+    };
+
+    document.getElementById("stopBtn").onclick = function () {
+      clearInterval(interval);
+      interval = null;
+    };
