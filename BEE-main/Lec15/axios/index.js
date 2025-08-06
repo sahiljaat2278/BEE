089637791194e8cc275e4e11dde6b212e066a265
sahil <sahil2278.be23@chitkara.edu.in>
2025-08-06 15:46:44 +0530

    new

--- BEE-main/Lec15/axios/index.js
+++ BEE-main/Lec15/axios/index.js
@@ -1,5 +1,6 @@
 const express= require('express');
 const app= express();
+app.use(express.json());
 app.use(express.static(__dirname+"/public"));
 app.post("/blog", (req, res) =>{
     console.log(req.body);
