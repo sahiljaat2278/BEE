a0166a292c6cdc38438dccd76706f9510a943428
sahil <sahil2278.be23@chitkara.edu.in>
2025-08-11 15:41:56 +0530

    new

--- BEE-main/Lec16/index.js
+++ BEE-main/Lec16/index.js
@@ -2,6 +2,41 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const Blogs = require('./model/user');
+
+app.post('/blog',async (req,res)=>{
+    let {title, body} = req.body;
+    let newBlog = new Blogs({
+        title: title,
+        body: body,
+        date: new Date()
+    });
+    await newBlog.save();
+    res.json({
+        success: true, 
+        data: newBlog,
+        message: "Blog created successfully"
+    });
+})
+app.get('/blog',async (req,res)=>{
+    let allBlogs = await Blogs.find();
+    res.json({
+        success: true,
+        data: allBlogs,
+    });
+});
+app.get('/blog/:id',async (req,res)=>{
+    let {id} = req.params;
+    let blog = await Blogs.findOne({_id: id});
+    res.json({
+        success: true,
+        data: blog,
+    });
+});
+
 
 
 app.listen(3000, () => {
