c0307cd151e230c121fb65d6fc82417309108a02
sahil <sahil2278.be23@chitkara.edu.in>
2025-08-12 15:17:11 +0530

    new

--- BEE-main/Lec16/index.js
+++ BEE-main/Lec16/index.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const Blogs = require('./model/user');
+const Blogs = require('./model/users');
 
 app.post('/blog',async (req,res)=>{
     let {title, body} = req.body;
@@ -35,6 +35,37 @@ app.get('/blog/:id',async (req,res)=>{
         success: true,
         data: blog,
     });
+});
+app.post("/users", async (req, res) => {
+  let { email, username, password } = req.body;
+  let newUser = new Users({
+    email: email,
+    username: username,
+    password: password
+  });
+  await newUser.save();
+  res.json({
+    success: true,
+    data: newUser,
+    message: "user added successfully!!!"
+  });
+});
+
+app.get("/users", async (req, res) => {
+  let allUsers = await Users.find();
+  res.json({
+    success: true,
+    data: allUsers
+  });
+});
+
+app.get("/users/:id", async (req, res) => {
+  let { id } = req.params;
+  let user = await Users.findOne({ _id: id });
+  res.json({
+    success: true,
+    data: user
+  });
 });
 
 
