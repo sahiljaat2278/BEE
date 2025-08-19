7ff579423d4ac19e49b0aa7fe423a89f3c597338
sahil <sahil2278.be23@chitkara.edu.in>
2025-08-19 15:29:44 +0530

    new

--- BEE-main/Lec20/index.js
+++ BEE-main/Lec20/index.js
@@ -2,22 +2,28 @@
 
 const express = require("express");
 const { m1, m2 } = require("./middleware/firstmiddleware");
+const { m3 } = require("./middleware/pathlevel");
+
 
 const app = express();
+
 
 app.use(express.static(__dirname + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(m1);
-app.use(m2);
-app.get("/health", (req, res) => {
+
+app.use("/api/users", userRoutes);
+
+app.get("/health", m3, (req, res, next) => {
    console.log("running controller function");
    res.json({ status: "OK",
     message: "Server is healthy",
     });
   
 });
-app.get("/home", (req, res) => {
+app.use(m2);
+app.get("/home", (req, res, next) => {
     console.log("running controller function");
     res.json({  success: true,
     message: "Welcome to the home page",
