e00942ef8be644af56c38517cfe0bd9ecb2ee582
sahil <sahil2278.be23@chitkara.edu.in>
2025-08-19 14:45:38 +0530

    new

--- BEE-main/Lec20/index.js
+++ BEE-main/Lec20/index.js
@@ -1,12 +1,21 @@
 //middleware - function which run on client requests before controller functions
 
 const express = require("express");
+const { m1, m2 } = require("./middleware/firstmiddleware");
 
 const app = express();
 
 app.use(express.static(__dirname + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(m1);
+app.use(m2);
+app.get("/health", (req, res) => {
+   console.log("running controller function");
+   res.json({ status: "OK",
+    message: "Server is healthy",
+    });
+});
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
