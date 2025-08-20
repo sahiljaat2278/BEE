5c35e6c6600977520d501015a28bd489f4cd7e25
sahil <sahil2278.be23@chitkara.edu.in>
2025-08-20 14:48:46 +0530

    new

--- BEE-main/Lec21/index.js
+++ BEE-main/Lec21/index.js
@@ -1,23 +1,59 @@
-const express = require("express");
-const { default: mongoose } = require("mongoose");
+const express = require('express');
 const app = express();
-app.get("/health", (req, res) => {
-    
-    res.json({ status: "OK", 
-        message: "Health check successful" });
+const mongoose = require('mongoose');
+
+const User = require('./model/user');
+app.use(express.json());
+
+
+
+app.get('/health', (req, res) => {
+    res.json({
+        status: 'ok',
+        message: 'Server is running'
+    });
 });
+
+//endpoint to create a new user in db
+app.post('/api/users/signup', async (req, res) => {
+    try {
+        let { name, email, password } = req.body;
+        let userExists = await User.findOne({ email });
+        if (userExists) {
+            return res.json({ success: false, 
+                message: "User already exists with this email please login" });
+        }
+        const user = new User({ name, email, password });
+        await user.save();
+        res.status(201).json(user);
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
+});
+
+
+app.get('/users', async (req, res) => {
+    try {
+        const users = await User.find();
+        res.json(users);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+
+
 
 
 mongoose.connect("mongodb://localhost:27017/blogapp", {
     
 })
 .then(() => {
-    console.log("Connected to MongoDB");
-})
-.catch((err) => {
-    console.error( err.message);
+    console.log('Connected to MongoDB');
+}).catch(err => {
+    console.error('Error connecting to MongoDB:', err);
 });
 
-app.listen(3000, () => {
-    console.log("Server is running on port http://localhost:3000");
+app.listen(3000,(req,res)=>{
+    console.log(`Server is running on port 3000`);
 });
