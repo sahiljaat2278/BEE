216857d23c6dd65a8eb302431bae1346ae0f09ff
sahil <sahil2278.be23@chitkara.edu.in>
2025-09-15 15:24:43 +0530

    new

--- BEE-main/Lec28/index.js
+++ BEE-main/Lec28/index.js
@@ -1,3 +1,12 @@
-function adduser(email, name, password) {
-    
+const { PrismaClient } = require('/generated/prisma');
+const prisma = new PrismaClient();
+ async function adduser(email, name, password) {
+    await prisma.user.create({
+        data: {
+            email: email,
+            name: name,
+            password: password
+        }
+    })
+
 }
