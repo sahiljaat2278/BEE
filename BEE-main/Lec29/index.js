2283f74a0fed189b20cbf568828e5a6db019451e
sahil <sahil2278.be23@chitkara.edu.in>
2025-09-16 15:18:19 +0530

    new

--- BEE-main/Lec29/index.js
+++ BEE-main/Lec29/index.js
@@ -1,16 +1,45 @@
-const { PrismaClient } = require('./generated/prisma');
+const {PrismaClient} = require('./generated/prisma');
 const prisma = new PrismaClient();
- async function adduser(email, name, password) {
-    await prisma.user.create({
-        data: {
-            email: email,
-            name: name,
-            password: password
+async function addUser(email,name,password){
+   let newUser = await prisma.user.create({
+    data:{
+       email:email,
+       name:name,
+       password:password
+    }
+
+  })
+  return newUser;
+}
+
+// addUser("vansh@gmail.com","vansh","12345")
+// .then(()=>{console.log("User added");})
+
+async function addTweet(content,userId){
+    let newTweet = await prisma.tweet.create({
+        data:{
+            content:content,
+            userId:userId
         }
     })
+    return newTweet;
 
 }
- adduser("sahilthory1@gmail.com", "sahil", "1234")
-.then(() => {
-    console.log("User added");
-})
+
+// addTweet("My first tweet",1)
+// .then(()=>{
+//     console.log("Tweet added");
+// })
+
+async function getUserTweet(userId){
+    let tweets = await prisma.tweet.findMany({
+        where:{
+            userId:Number(userId)
+        }
+    })
+    return tweets;
+}
+
+    getUserTweet(1).then(tweets => {
+        console.log(tweets);
+    });
