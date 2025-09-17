be7508903e380361560701077235a3dae258ae7d
sahil <sahil2278.be23@chitkara.edu.in>
2025-09-17 15:43:38 +0530

    new

--- BEE-main/Lec29/index.js
+++ BEE-main/Lec29/index.js
@@ -2,74 +2,74 @@ const {PrismaClient} = require('./generated/prisma');
 const prisma = new PrismaClient();
 
 //Add User
-async function addUser(email,name,password){
-   let newUser = await prisma.user.create({
-    data:{
-       email:email,
-       name:name,
-       password:password
-    }
+// async function addUser(email,name,password){
+//    let newUser = await prisma.user.create({
+//     data:{
+//        email:email,
+//        name:name,
+//        password:password
+//     }
 
-  })
-  return newUser;
-}
+//   })
+//   return newUser;
+// }
 
-// addUser("sahil@gmail.com","sahil","12345")
+// addUser("yug@gmail.com","yug","12345")
 // .then(()=>{console.log("User added");})
 
 //Add Tweet
-async function addTweet(content,userId){
-    let newTweet = await prisma.tweet.create({
-        data:{
-            content:content,
-            userId:userId
-        }
-    })
-    return newTweet;
+// async function addTweet(content,userId){
+//     let newTweet = await prisma.tweet.create({
+//         data:{
+//             content:content,
+//             userId:userId
+//         }
+//     })
+//     return newTweet;
 
-}
+// }
 
-// addTweet("My first tweet",3)
+// addTweet("My fourth tweet",7)
 // .then(()=>{
 //     console.log("Tweet added");
 // })
 
 //Get User Tweet
-async function getUserTweet(userId){
-    let tweets = await prisma.tweet.findMany({
-        where:{
-            userId:Number(userId)
-        }
-    })
-    return tweets;
-}
+// async function getUserTweet(userId){
+//     let tweets = await prisma.tweet.findMany({
+//         where:{
+//             userId:Number(userId)
+//         }
+//     })
+//     return tweets;
+// }
 
-   //  getUserTweet(1).then(tweets => {
-   //      console.log(tweets);
-   //  });
+    // getUserTweet(1).then(tweets => {
+    //     console.log(tweets);
+    // });
 
 //Update Tweet
-async function updateTweet(tweetId,userId,updatedContent){
-   let tweet = await prisma.tweet.findUnique({
-      where: {
-         id: Number(tweetId)
-      }
-   });
-   if(!tweet){
-      return "Tweet not found";
-   }
-   if(tweet.userId != Number(userId)){
-      return "user can not update";
-   }
-   await prisma.tweet.update({
-      where:{
-         id:Number(tweetId)
-      },
-      data:{ 
-         content:updatedContent
-      }
-   });
-}
+// async function updateTweet(tweetId,userId,updatedContent){
+//    let tweet = await prisma.tweet.findUnique({
+//       where: {
+//          id: Number(tweetId)
+//       }
+//    });
+//    if(!tweet){
+//       return "Tweet not found";
+//    }
+//    if(tweet.userId != Number(userId)){
+//       return "user can not update";
+//    }
+//    await prisma.tweet.update({
+//       where:{
+//          id:Number(tweetId)
+//       },
+//       data:{ 
+//          content:updatedContent
+//       }
+//    });
+// }
 // updateTweet("1","1","Updated tweet content")
 // .then(()=>{
 //    console.log("Tweet updated");
@@ -77,13 +77,13 @@ async function updateTweet(tweetId,userId,updatedContent){
 
 
 //Delete User
-async function deleteUser(userId){
-   await prisma.user.delete({
-      where:{
-         id:Number(userId)
-      }
-   });
-}
+// async function deleteUser(userId){
+//    await prisma.user.delete({
+//       where:{
+//          id:Number(userId)
+//       }
+//    });
+// }
 
 // deleteUser(1)
 // .then(()=>{
@@ -94,14 +94,26 @@ async function deleteUser(userId){
 
    async function printAllUserEmailName() {
       const users = await prisma.user.findMany({
-         select: {
-            email: true,
-            name: true
+         // select: {
+         //    email: true,
+         //    name: true,
+         //    tweets: {
+         //       select: {
+         //          content: true
+         //       }
+         //    }
+         // }
+         include: {
+            tweets: {
+               select: {
+                  content: true
+               }
+            }
          }
       });
-      users.forEach(user => {
-         console.log(`Email: ${user.email}, Name: ${user.name}`);
-      });
+     return users;
    }
 
-   printAllUserEmailName();
+       printAllUserEmailName()
+       .then((data) => { console.log(JSON.stringify(data,null,2));})
+          
