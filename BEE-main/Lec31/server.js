29203d4a6dc823218edfd030460679ed56b59940
sahil <sahil2278.be23@chitkara.edu.in>
2025-09-29 15:29:27 +0530

    sahil

---
+++ BEE-main/Lec31/server.js
@@ -0,0 +1,18 @@
+const { WebSocketServer } = require( 'ws');
+
+const ws = new WebSocketServer({ port: 8015 });
+//event handler
+ws.on('connection', function connection(socket) {
+    console.log(socket);
+    
+    setInterval(() => {
+        socket.send(new Date().toLocaleTimeString());
+    
+    socket.send('Reliance stock price is ' +" "+ Math.random());
+    }, 500);
+    socket.on('message', function message(data) {
+        console.log(data.toString());
+    });
+})
+
+  ///app.get("/",(req,res)=>{});
