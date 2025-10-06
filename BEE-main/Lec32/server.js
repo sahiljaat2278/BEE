b3ae01704fa38f3d58926241a9ebde85380a79e3
sahil <sahil2278.be23@chitkara.edu.in>
2025-10-06 14:51:07 +0530

    new

--- BEE-main/Lec32/server.js
+++ BEE-main/Lec32/server.js
@@ -2,12 +2,26 @@ const {WebSocketServer} = require('ws');
 const ws = new WebSocketServer({port:8015});
 
 
+// ws.on('connection', function(socket) {
+//     console.log("User connected");
+//     allsocket.push(socket);
+//     socket.on("message", function(message) {
+//         console.log("Received: " + message.toString());
+//         if(message.toString() === "Ping") {
+//             socket.send("Pong");
+//         }
+//     });
+// })
+
+
+let allsocket = [];
 ws.on('connection', function(socket) {
     console.log("User connected");
+    allsocket.push(socket);
     socket.on("message", function(message) {
-        console.log("Received: " + message.toString());
-        if(message.toString() === "Ping") {
-            socket.send("Pong");
-        }
+        console.log("Received: " +" "+ message.toString());
+        allsocket.forEach((s) => {
+            s.send(message.toString());
+        })
     });
 })
