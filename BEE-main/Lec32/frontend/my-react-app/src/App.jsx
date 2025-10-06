439e89d086ada7c399776efc2a0cb95e55ca7e24
sahil <sahil2278.be23@chitkara.edu.in>
2025-10-06 15:17:18 +0530

    new

--- BEE-main/Lec32/frontend/my-react-app/src/App.jsx
+++ BEE-main/Lec32/frontend/my-react-app/src/App.jsx
@@ -1,13 +1,21 @@
 import { useEffect } from 'react'
 function App() {
+  //useEffect is a hook use to do side effect in react
+  let[ws,setWs]=useState(null);
   useEffect(() => {
     const socket = new WebSocket("ws://localhost:8015");
+    setWs(socket);
+  }, [])
+  function SendMessage(){
+    ws.send("Ping");
+  }
 
-  }, []);
 
   return (
     <>
     <h1>Ping Pong</h1>
+    <input type="text" />
+    <button onClick={SendMessage}>Send</button>
     </>
   )
 }
