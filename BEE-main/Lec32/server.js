ba4c157a020c583af4df7fb8ed6f5833a272e38d
sahil <sahil2278.be23@chitkara.edu.in>
2025-10-06 14:34:07 +0530

    new

--- BEE-main/Lec32/server.js
+++ BEE-main/Lec32/server.js
@@ -4,4 +4,10 @@ const ws = new WebSocketServer({port:8015});
 
 ws.on('connection', function(socket) {
     console.log("User connected");
+    socket.on("message", function(message) {
+        console.log("Received: " + message.toString());
+        if(message.toString() === "Ping") {
+            socket.send("Pong");
+        }
+    });
 })
