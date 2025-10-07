68cac7f591e6fbbd39fdb0e6f94fac9fbf6858f2
sahil <sahil2278.be23@chitkara.edu.in>
2025-10-07 14:55:31 +0530

    new

---
+++ BEE-main/Lec33/frontend/my-react-app/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { useEffect } from 'react'
+
+function App() {
+  //use Effect --> hook use to do side-effect in react
+  let [ws,setWs] = useState(null);
+  useEffect(()=>{
+    let socket = new WebSocket("ws://localhost:8015");
+    socket.onmessage=((e)=>{
+      console.log(e.data);
+    })
+    setWs(socket);
+  },[]);
+  
+
+  function sendMessage(){
+    ws.send("ping");
+  }
+
+  return (
+    <>
+    <h1>Ping Pong</h1>
+    <input type="text" />
+    <button onClick={sendMessage}>Send</button>
+ 
+    </>
+  )
+}
+
+export default App;
