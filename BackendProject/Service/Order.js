a1ba0465dce4a6684151027395817f7ab596e7d7
sahil <sahil2278.be23@chitkara.edu.in>
2025-10-28 15:04:15 +0530

    new

--- BackendProject/Service/Order.js
+++ BackendProject/Service/Order.js
@@ -1,3 +1,33 @@
 class OrderBook {
-    
+    constructor(symbol="BTCUSD") {
+        this.bids=[],
+        this.asks=[],
+        this.nextId=1,
+        this.lastTradePrice=null
+
+    }
+    //helper
+    _genOrderID(){
+        return this.nextId++;
+    }
+    _sort(sides){
+        if(sides==="BUY"){
+            this.bids.sort((a,b)=>{
+                if(a.price !=b.price){
+                    return b.price - a.price
+                }
+                return a.timestamp - b.timestamp
+            })
+        }else{
+            this.asks.sort((a,b)=>{
+                if(a.price !=b.price){
+                    return a.price - b.price
+                }
+                return a.timestamp - b.timestamp
+            })
+        }
+    }
 }
+//_ is just used so other devlopers know that this is a private function or variable
+//if a function or variable starts with _(private)
+///Let OrderBook = new OrderBook("BTCUSD")
