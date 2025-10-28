05c7f34efece153b3fe2f61970143dafe283b205
sahil <sahil2278.be23@chitkara.edu.in>
2025-10-28 15:13:55 +0530

    new

--- BackendProject/Service/Order.js
+++ BackendProject/Service/Order.js
@@ -31,3 +31,15 @@ class OrderBook {
 //_ is just used so other devlopers know that this is a private function or variable
 //if a function or variable starts with _(private)
 ///Let OrderBook = new OrderBook("BTCUSD")
+
+
+let BTCUSDOrderBook = new OrderBook()
+
+BTCUSDOrderBook.bids.push({orderId:2,side:"BUY",type:"MARKET",price:100,quantity:10,timestamp:Date.now(),user:"Sahil"})
+
+BTCUSDOrderBook.bids.push({orderId:2,side:"BUY",type:"LIMIT",price:101,quantity:10,timestamp:Date.now(),user:"Aman"})
+
+BTCUSDOrderBook.asks.push({orderId:2,side:"SELL",type:"LIMIT",price:105,quantity:10,timestamp:Date.now(),user:"Ankit"})  
+
+BTCUSDOrderBook._sort("BUY")
+console.log(BTCUSDOrderBook.bids);
