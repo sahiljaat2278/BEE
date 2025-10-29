f19a1f9f80071901a305f90cb0089c81ddf07316
sahil <sahil2278.be23@chitkara.edu.in>
2025-10-29 14:46:19 +0530

    new

--- BackendProject/Service/Order.js
+++ BackendProject/Service/Order.js
@@ -28,15 +28,57 @@ class OrderBook {
         }
     }
     ////Public function
-_PlaceOrder(){
+    /*
+    1. create new order {orderID,side,type,price?,orignqty,remainingquantity,execqty,timestamp,user}
+    2. match type if ==market ,call market, else call limit match
+     */
+_PlaceOrder(symbol,side,type,price=null,quantity,user){
+    // Basic Validation 
+    /**
+     bids:[]sorted descending,
+     asks:[]sorted ascending
 
+     1. type : buy | sell
+     2. if buy start buying from ask array starting from index 0.
+      loop while order.remainQTY>0 && ask.length>0
+      buy min(order.remainingQty,ask[0].remainingQty)
+      update remaining qty and executedqty from both side
 
+    */
+        
+    
+    let order={
+        symbol: this.symbol,
+        side: side,
+        type: type,
+        price: price,
+        orignlQty: quantity,
+        remainQty: quantity,
+        exectedQty: 0,
+        timestamp: Date.now(),
+        user: user
+    };
+   
+    if(type === "MARKET"){
+        this._marketMatch(order);
+    }else{
+        this._limitMatch(order);
+    }
 }
-_marketMatch(){
+_marketMatch(order){
+    if(type==="BUY"){
+        let askArr= this.asks
+        let top = askArr[0];
+        while(order.remainQty>0 && askArr.length>0){
+             let orderfill= Math.min(order.remainQty,top.remainQty);
+             order.exectedQty = order.exectedQty + orderfill;
 
-
+             top.exectedQty = top.exectedQty + orderfill;
+             top.remainQty = top.remainQty - orderfill;
+        }
+    }
 }
-_limitMatch(){
+_limitMatch(order){
 
 
 
